b12035213e6bfc3adcc6ee8f65276772c05ddd83
greenhornet63603 <greenhornet63603@gmail.com>
2026-05-13 22:51:40 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,20 +3,22 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27400172"))
+API_HASH = environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8396814092:AAHtUGEPciF5LdiCo572-TczY3EZwx0Otis")
 
-OWNER = int(environ.get("OWNER", ""))
+OWNER = int(environ.get("OWNER", "7540570087"))
 CREDIT = environ.get("CREDIT", "")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7540570087,7061670859').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7540570087,7061670859').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
+
